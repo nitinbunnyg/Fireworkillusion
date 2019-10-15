2c37bd7f8a033f03a2f8db0e969225d93fcff915
Abhishek Kasireddy <akasireddy99@gmail.com>
2019-10-15 22:46:50 +0530

    Ayayaya

--- src/components/utils/searchbar.js
+++ src/components/utils/searchbar.js
@@ -1,42 +1,42 @@
 import React, { Component } from 'react'
 
 class SearchBar extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      book: ''
-    }
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			book: ''
+		}
+	}
 
-  handleSearch (e) {
-    this.setState({ book: e.target.value })
-  }
+	handleSearch(e) {
+		this.setState({ book: e.target.value })
+	}
 
-  handleGoClick () {
-    if (!this.props) {
-    //   this.props.actions.fetchBook(this.state)
-    }
-  }
+	handleGoClick() {
+		if (!this.props) {
+			//   this.props.actions.fetchBook(this.state)
+		}
+	}
 
-  render () {
-    return (
-      <div className='searchbar-container'>
-        <form onSubmit={e => e.preventDefault()}>
-          <input
-            type='text'
-            size='45'
-            placeholder='Search Library'
-            onChange={this.handleSearch.bind(this)}
-            value={this.state.book} />
-          <button
-            type='submit'
-            onClick={this.handleGoClick.bind(this)}>
-            Search
+	render() {
+		return (
+			<div className='searchbar-container'>
+				<form onSubmit={e => e.preventDefault()}>
+					<input
+						type='text'
+						size='45'
+						placeholder='Search Library'
+						onChange={this.handleSearch.bind(this)}
+						value={this.state.book} />
+					<button
+						type='submit'
+						onClick={this.handleGoClick.bind(this)}>
+						Search
           </button>
-        </form>
-      </div>
-    )
-  }
+				</form>
+			</div>
+		)
+	}
 }
 
 export default SearchBar
